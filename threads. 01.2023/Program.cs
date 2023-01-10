437cbabfb5db42bd107c4f493265ef3b198fc666
Gilman M <gilman.mm@parmalogica.ru>
2023-01-10 12:44:06 +0500

    Вынесение в отдельный файл

--- threads. 01.2023/Program.cs
+++ threads. 01.2023/Program.cs
@@ -1,31 +1,15 @@
-﻿#region simple-thread
-
-using System.Collections.Concurrent;
-
-namespace threads._01._2023;
+﻿namespace threads._01._2023;
 
 static class Program
 {
     public static void Main()
     {
-        using (BlockingCollection<int> collection = new BlockingCollection<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 })
-        {
-            Action read = () =>
-            {
-                while (collection.TryTake(out var item))
-                    Console.WriteLine($"Thread ID: {Environment.CurrentManagedThreadId}. Value: {item}");
-            };
-            
-            Console.WriteLine($"Parent thread ID: {Environment.CurrentManagedThreadId} \n");
+        // 1е задание
+        Task1_Create_child_thread.Execute();
+        
+        //
 
-            var mainTask = new Task(read);
-            var childTask = Task.Run(read);
-            mainTask.RunSynchronously();
-
-            Task.WaitAll(childTask, mainTask);
-        }
     }
     
 }
 
-# endregion
---
+++ threads. 01.2023/Task1.Create_child_thread.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Concurrent;
+
+namespace threads._01._2023;
+
+public class Task1_Create_child_thread
+{
+    public static void Execute()
+    {
+        using (BlockingCollection<int> collection = new BlockingCollection<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 })
+        {
+            Action read = () =>
+            {
+                while (collection.TryTake(out var item))
+                    Console.WriteLine($"Current thread ID: {Environment.CurrentManagedThreadId}. Value: {item}");
+            };
+            
+            Console.WriteLine($"Parent thread ID: {Environment.CurrentManagedThreadId} \n");
+
+            var mainTask = new Task(read);
+            var childTask = Task.Run(read);
+
+            mainTask.RunSynchronously();
+
+            Task.WaitAll(childTask, mainTask);
+        }
+    }
+}
