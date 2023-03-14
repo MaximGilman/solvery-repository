6770675b12cbe368057bb48f4c0afd5f5286e55f
Gilman M <gilman.mm@parmalogica.ru>
2023-03-14 23:34:35 +0500

    Создание проекта

---
+++ Utils/Guards/NetworkGuards.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+
+namespace Utils.Guards;
+
+public partial class Guard
+{
+
+    /// <summary>
+    /// Проверить, что строка не пустая.
+    /// </summary>
+    /// <param name="port">Текущее значение.</param>
+    public static void IsValidPort(int port)
+    {
+        Guard.IsGreater(port, IPEndPoint.MinPort);
+        Guard.IsLess(port, IPEndPoint.MaxPort);
+    }
+}
