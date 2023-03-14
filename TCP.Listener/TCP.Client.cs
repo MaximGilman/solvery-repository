d0a5c15982389fd8ad8b71cbb680a5f3456dc343
Gilman M <gilman.mm@parmalogica.ru>
2023-03-14 23:59:17 +0500

    Вынесение двух решений

---
+++ TCP.Listener/TCP.Client.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using Utils.Guards;
+
+namespace TCP.Listener;
+
+public static class TCP_Client
+{
+    private const int PORT = 13001;
+
+    public static async Task Execute(CancellationToken cancellationToken)
+    {
+        Guard.IsValidPort(PORT);
+
+        try
+        {
+            var ipEndPoint = new IPEndPoint(IPAddress.Loopback, PORT);
+            Console.WriteLine("Start working...");
+            using var client = new TcpClient(ipEndPoint); // Создали клиента и забайндили на эндпоинт
+            Console.WriteLine("TCP client bound");
+            await client.ConnectAsync(ipEndPoint, cancellationToken);
+            Console.WriteLine($"TCP client connected: { client.Connected}");
+
+            await using var stream = client.GetStream();
+            var bytesRead = 0;
+            var buffer = new byte[512];
+
+            Console.WriteLine("Receiving buffer and stream created");
+
+
+            Console.WriteLine($"Start listening on {client.Client.RemoteEndPoint}");
+            do
+            {
+                bytesRead += await stream.ReadAsync(buffer, cancellationToken);
+                Console.WriteLine($"bytes read: {bytesRead}");
+
+            } while (client.Available > 0);
+        }
+
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+    }
+
+}
---
+++ TCP.Listener/TCP.Listener.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using Utils.Guards;
+
+namespace TCP.Listener;
+
+public static class TCP_Listener
+{
+    private const int PORT = 13000;
+    public static async Task Execute(CancellationToken cancellationToken)
+    {
+        Guard.IsValidPort(PORT);
+
+        var listener = new TcpListener(new IPEndPoint(IPAddress.Loopback, PORT));
+
+        try
+        {
+            listener.Start();
+
+            var bytes = new byte[256];
+            string data = null;
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                Console.Write("Waiting for a connection... ");
+
+                using var client = await listener.AcceptTcpClientAsync(cancellationToken);
+                Console.WriteLine("Connected");
+                var stream = client.GetStream();
+
+                int i;
+
+                while ((i = await stream.ReadAsync(bytes, cancellationToken)) != 0)
+                {
+                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                    Console.WriteLine("Received: {0}", data);
+                }
+            }
+        }
+        finally
+        {
+            listener.Stop();
+        }
+    }
+}
