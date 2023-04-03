25a3c13bfeb0251b467c845b974cd2c1c0c3a278
Gilman M <gilman.mm@parmalogica.ru>
2023-04-03 19:28:10 +0500

    Revert "Удаление задание 1"
    This reverts commit 72afcd77d68a4aa4487743178896005bf6f3ac9a.

---
+++ TCP.Listener/Task 1. TcpListener/Utils/TcpPortUtils.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.NetworkInformation;
+
+namespace TCP.Listener.Utils;
+
+public static class TcpPortUtils
+{
+    public static bool IsTcpPortAvailable(int portNumber)
+    {
+        return IPGlobalProperties.GetIPGlobalProperties().GetActiveTcpListeners().All(p => p.Port != portNumber);
+    }
+}
