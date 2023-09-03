c336c612a442e5df769172017e804bd8d43c23bd
Gilman M.M <mmgilman@xrm.ru>
2023-09-03 15:02:44 +0500

    Удаление лишних проектов

--- TCP/TCP.Task2.Listener/ITcpListener.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TCP.Task2.Listener;
-
-public interface ITcpListener
-{
-    internal const int BYTE_BUFFER_SIZE = 4096 * 8;
-
-    public Task HandleReceiveFile(string fileNameTarget, CancellationToken cancellationToken);
-}
