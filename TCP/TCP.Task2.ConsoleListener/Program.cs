ad416c0e76333676f996c8ec7622d553eb21ec15
Gilman M.M <mmgilman@xrm.ru>
2023-10-16 18:45:23 +0500

    Удаление файлов старой реализации

--- TCP/TCP.Task2.ConsoleListener/Program.cs
+++ TCP/TCP.Task2.ConsoleListener/Program.cs
@@ -1,11 +1,1 @@
-﻿using Microsoft.Extensions.Logging;
-using TCPViaUDP.Helpers;
-
-var loggerFactory = LoggerFactory.Create(builder => { builder.AddConsole(); });
-var logger = loggerFactory.CreateLogger<TCPViaUDPReceiver>();
-
-const int PORT_RECEIVE = 60381;
-
-var newVersionOfUdpReceiver = new TCPViaUDPReceiver(PORT_RECEIVE, logger);
-
-await newVersionOfUdpReceiver.HandleAllReceiveAsync(CancellationToken.None);
+﻿Console.WriteLine("TBD");
